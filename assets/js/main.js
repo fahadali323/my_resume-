136534c152d81bcf00bc3f1b262497e7aa743de0
fahadali323 <afahad5312@gmail.com>
2023-11-29 22:40:11 -0500

    added menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,24 @@
-/*=============== SHOW MENU ===============*/
+// main.js
+document.addEventListener('DOMContentLoaded', function () {
+    const navMenu = document.getElementById('nav-menu');
+    const navToggle = document.getElementById('nav-toggle');
+    const navClose = document.getElementById('nav-close');
+  
+    /* Validate if constants exist */
+    if (navToggle) {
+      navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+      });
+    }
+  
+    /* Validate if constants exist */
+    if (navClose) {
+      navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+      });
+    }
+  });
+  console.log("Main.js is loaded!");
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
